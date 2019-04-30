765b9b677f4f29bd0c961d008b44e57370c536ac
Jacob Parra <jacobrpg@gmail.com>
2019-04-30 03:08:59 -0400

    Adds weather manager with cache

---
+++ src/city-manager.ts
@@ -0,0 +1,44 @@
+import { darkSky, Weather } from './darksky';
+import { cache } from './cache';
+import { cities } from './constants';
+
+export interface City {
+  name: string;
+  latitude: number;
+  longitude: number;
+  weather?: Weather;
+}
+
+class CityManager {
+
+  async getAllCitiesWeather(): Promise<City[]> {
+    const promises = cities.map(async (city) => {
+      city.weather = await this.getWeather(city);
+      return city;
+    });
+    return Promise.all(promises);
+  }
+
+  async getWeather(city: City): Promise<Weather> {
+    const weather = await cache.getCityWeather(city.name);
+    if (Object.keys(weather).length === 0) {
+      return this.updateWeather(city);
+    }
+    return weather;
+  }
+
+  async updateWeather(city: City): Promise<Weather> {
+    try {
+      const weather = await darkSky.getCurrentWeather(city.latitude, city.longitude);
+      cache.setCityWeather(city.name, weather);
+      return weather;
+    } catch (e) {
+      if (e.name === 'DarkSkyRandomError') {
+        return this.updateWeather(city);
+      }
+      throw e;
+    }
+  }
+}
+
+export const cityManager = new CityManager();
---
+++ src/constants.ts
@@ -0,0 +1,34 @@
+import { City } from './city-manager';
+
+export const cities: City[] = [
+  {
+    name: 'Santiago (CL)',
+    latitude: -33.45,
+    longitude: -70.666667
+  },
+  {
+    name: 'Zurich (CH)',
+    latitude: 47.366667,
+    longitude: 8.55
+  },
+  {
+    name: 'Auckland (NZ)',
+    latitude: -36.840556,
+    longitude: 174.74
+  },
+  {
+    name: 'Sydney (AU)',
+    latitude: -33.865,
+    longitude: 151.209444
+  },
+  {
+    name: 'Londres (UK)',
+    latitude: 51.507222,
+    longitude: -0.1275
+  },
+  {
+    name: 'Georgia (USA)',
+    latitude: 33,
+    longitude: -83.5
+  }
+];
